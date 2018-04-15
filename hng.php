3d093b7ad87e8af352e56d407737683f251044e8
Hephzibah Ajah <hephzibah@Hephzibahs-MacBook-Pro.local>
2018-04-15 18:46:03 +0100

    Hello

---
+++ hng.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>HNG Internship</title>
+    <style>
+      div
+      {
+
+        font-weight: 300;
+      }
+
+
+    </style>
+    <body>
+      <center>
+
+        <div style="padding-top:109px">
+          <h1 style="color:#E5E5E5;"> Time </h1>
+          <?php
+          echo date("h:i:a");
+          ?>
+        </div>
+
+      </center>
+
+    </body>
+</html>
